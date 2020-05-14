7903eb4e4e952aa037ba4bb8f12baec2b8c7ba15
Sergey Zorin <sergey.zorin@outlook.com>
2020-05-14 14:28:17 +0300

    Rework status icon rendering.

---
+++ src/ToDo/Views/Shared/Components/StatusIcon/StatusIconViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ToDo.Views.Shared.Components.StatusIcon
+{
+    public class StatusIconViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(DateTimeOffset dueDate, bool isCompleted)
+        {
+            var model = new StatusIconViewComponentModel
+            {
+                DueDate = dueDate,
+                IsCompleted = isCompleted,
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/ToDo/Views/Shared/Components/StatusIcon/StatusIconViewComponentModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ToDo.Views.Shared.Components.StatusIcon
+{
+    public class StatusIconViewComponentModel
+    {
+        public DateTimeOffset DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
