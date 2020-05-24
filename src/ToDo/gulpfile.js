8f5be4d7677006f4f79bccb1bf728fa4f9866739
Sergey Zorin <sergey.zorin@outlook.com>
2020-05-24 18:21:32 +0300

    Node modules and Gulp.

---
+++ src/ToDo/gulpfile.js
@@ -0,0 +1,54 @@
+﻿/// <binding BeforeBuild='build' Clean='clean' />
+"use strict";
+
+const { src } = require("gulp");
+const { dest } = require("gulp");
+const { parallel } = require("gulp");
+const { series } = require("gulp");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
+const del = require("del");
+
+var paths = {
+    webroot: "./wwwroot/",
+    modules: "./node_modules/"
+};
+
+paths.dest = paths.webroot + "dest/";
+paths.src = paths.webroot + "src/";
+
+function css() {
+    return src(`${paths.src}**/*.css`)
+        .pipe(cssmin())
+        .pipe(rename(function (path) {
+            path.extname = ".min.css";
+        }))
+        .pipe(dest(paths.dest))
+        .pipe(src(`${paths.src}**/*.css`))
+        .pipe(dest(paths.dest));
+}
+
+function modules(done) {
+    function font_awesome(done) {
+        function css() {
+            return src(`${paths.modules}@fortawesome/fontawesome-free/css/*`)
+                .pipe(dest(`${paths.dest}font-awesome/css`))
+        }
+
+        function fonts() {
+            return src(`${paths.modules}@fortawesome/fontawesome-free/webfonts/*`)
+                .pipe(dest(`${paths.dest}font-awesome/webfonts`))
+        }
+
+        return parallel(css, fonts)(done);
+    }
+
+    return parallel(font_awesome)(done);
+}
+
+function clean() {
+    return del(paths.dest);
+}
+
+exports.build = parallel(css, modules);
+exports.clean = clean;
